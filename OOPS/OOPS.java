185f9bcc43eeff286ff500b41835e4391a167ca7
Varan singh <varansingh2409@gmail.com>
2026-08-16 23:49:20 +0530

    oops concept

---
+++ OOPS/OOPS.java
@@ -0,0 +1,80 @@
+/*class Pen{
+    String color;
+    String type;
+
+    public void write(){
+        System.out.println("writing something");
+    }
+    public void printColor(){
+        System.out.println(this.color);
+        System.out.println(this.type);
+    }
+}
+
+class Student{
+ int age ;
+
+ public void StudentInfo() {
+    System.out.println(this.name);
+        System.out.println(this.age);
+ }
+ Student(){
+     
+ }
+ Student(Student s2 ){
+   // System.out.println("sahil nahar is a gay ");
+   this.name=s2.name;
+   this.age=s2.age;
+ }
+
+}
+public class OOPS{
+    public static void main(String[] args) {
+        // Pen pen1=new Pen();
+        // pen1.color="blue";
+        // pen1.type="gel";
+
+        // pen1.write();
+
+        // Pen pen2=new Pen();
+        // pen2.color="red";
+        // pen2.type="dot";
+
+        // pen1.printColor();
+        // pen2.printColor();
+
+     Student s1=new Student();
+       s1.name="sahil nahar";
+       s1.age=26;
+
+       Student s2 =new Student(s1);
+
+        s2.StudentInfo();
+    }
+}*/
+//POLYMORPHISM//
+
+/*class Student {
+String name;
+int age ;
+
+public void printaInfo(String name){
+    System.out.println(name);
+}
+public void printaInfo(int age){
+    System.out.println(age);
+}
+public void printaInfo(String name,int age){
+    System.out.println(name +" "+ age);
+}
+
+}
+public class OOPS{
+    public static void main(String[] args) {
+        Student s1=new Student();
+        s1.name="THE SAHIL NAHAR";
+        s1.age=26;
+
+        s1.printaInfo(s1.name,s1.age);
+    }
+}*/
